c0e26b23503fad9c2d35017eaaf686c6ae301607
bryant <bryant@defrag.in>
2013-08-23 12:20:31 -0400

    csv parser from real world haskell

---
+++ csv.hs
@@ -0,0 +1,13 @@
+import Text.ParserCombinators.Parsec
+
+csvFile = endBy line eol
+line = sepBy cell (char ',')
+cell = many (noneOf ",\n")
+eol = char '\n'
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
+
+main = do
+    raw <- getContents
+    putStrLn . show $ either (\_ -> [["failed."]]) id $ parseCSV raw
