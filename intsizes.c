b2ecccc58fab469aae183df160f682801c785bb0
bryant <bryant@defrag.in>
2013-09-04 14:21:58 -0400

    c int sizes

---
+++ intsizes.c
@@ -0,0 +1,8 @@
+int main() {
+    printf("short:\t%lu\n"
+           "int:\t%lu\n"
+           "long:\t%lu\n"
+           "long int\t%lu\n",
+           sizeof(short), sizeof(int), sizeof(long), sizeof(long int));
+    return 0;
+}
