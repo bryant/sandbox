a82b1eeb30af7fc067e54c9618fd0cdfb9ae684f
bryant <bryant@defrag.in>
2014-02-05 14:52:11 -0500

    jugglefest in haskell!
    - http://www.yodlecareers.com/puzzles/jugglefest.html
    - took way too long to write, takes way too long to run.
    - lesson learned: think as high level as possible when writing haskell, either
      by thinking functionally in terms of pure funcs or composing the task into
      english. *avoid thinking of low level.*

---
+++ jugglers.hs
@@ -0,0 +1,98 @@
+import Data.List (minimumBy)
+import Data.Array (Array, (//), assocs, array, (!))
+import Data.Attoparsec.Char8 (char, space, (<?>), sepBy, sepBy1, parseOnly,
+                              decimal)
+import qualified Data.ByteString as B
+
+type HEP = Int
+type JugglerID = Int
+type TeamID = Int
+data Juggler = Juggler { _jid :: JugglerID, jhep :: [HEP], prefs :: [TeamID] }
+data Team = Team { roster :: [Juggler], maxjugs :: Int, thep :: [HEP]}
+
+instance Show Juggler where show (Juggler id _ _) = 'J' : show id
+instance Show Team where show (Team rs _ _) = show rs
+instance Eq Juggler where (Juggler id _ _) == (Juggler id' _ _) = id == id'
+
+cons_snd x (y, xs) = (y, x : xs)
+
+drop_choice jugg@Juggler{prefs=prefs} = jugg { prefs = tail prefs }
+
+compat :: Team -> Juggler -> HEP
+compat Team{thep=hep} Juggler{jhep=hep'} = sum $ zipWith (*) hep hep'
+
+worst_on :: Team -> Juggler
+worst_on team@Team{roster=rs} = minimumBy cmpt rs
+    where cmpt x y = compare (compat team x) (compat team y)
+
+drop_worst_in :: Team -> Team
+drop_worst_in team@Team{roster=rs} = team { roster = filter (/= w) rs }
+    where w = worst_on team
+
+round_robin :: Array TeamID Team -> [Juggler] -> (Array TeamID Team, [Juggler])
+round_robin ts [] = (ts, [])
+round_robin ts (j@(Juggler _ _ []):js) = j `cons_snd` round_robin ts js
+round_robin ts (j@(Juggler _ _ (tid:tids)):js) = case overfull jhired of
+    False -> round_robin (ts // [(tid, jhired)]) js
+    True -> (ts // [(tid, jhired')]) `reject` (worst_on jhired)
+    where
+    prefer = ts ! tid
+    jhired = prefer { roster = (drop_choice j) : roster prefer }
+    jhired' = drop_worst_in jhired
+
+    overfull Team{roster=rs, maxjugs=mx} = length rs > mx
+    reject ts w@(Juggler _ _ []) = w `cons_snd` round_robin ts js
+    reject ts w = round_robin ts $ js ++ [w]
+
+donate_rejects :: Array TeamID Team -> [Juggler] -> Array TeamID Team
+donate_rejects ts [] = ts
+donate_rejects ts (j:js) = donate_rejects ts' js
+    where
+    ts' = ts // [(best_fit_id, jhired)]
+    avail = filter (is_full . snd) $ assocs ts
+    (best_fit_id, best_fit) = minimumBy better_fit avail
+    jhired = best_fit { roster = j : roster best_fit }
+    better_fit (_, t) (_, t') = compare (compat t j) (compat t' j)
+    is_full Team{roster=rs, maxjugs=mx} = length rs == mx
+
+newline = char '\n'
+
+read_id prefix = flip (<?>) "read_id" $ char prefix >> decimal
+
+read_hep = flip (<?>) "read_hep" $ do
+    h <- char 'H' >> char ':' >> decimal
+    e <- space >> char 'E' >> char ':' >> decimal
+    p <- space >> char 'P' >> char ':' >> decimal
+    return [h, e, p]
+
+-- C C1999 H:9 E:5 P:9
+circuit_line = flip (<?>) "circuit_line" $ do
+    tid <- char 'C' >> space >> read_id 'C'
+    hep <- space >> read_hep
+    return (tid, hep)
+
+-- J J0 H:7 E:6 P:0 C453,C1706,C318,C271,C1958,C1051,C241,C1736,C304,C518
+juggler_line = flip (<?>) "juggler_line" $ do
+    jid <- char 'J' >> space >> read_id 'J'
+    space
+    hep <- read_hep
+    space
+    prefs <- read_id 'C' `sepBy` char ','
+    return $ Juggler jid hep prefs
+
+specs = do
+    (ids, theps) <- unzip `fmap` (circuit_line `sepBy1` newline)
+    newline >> newline
+    js <- juggler_line `sepBy1` newline
+    let n = length js `quot` length ids
+    let ts = array (minimum ids, maximum ids) . zip ids $ map (Team [] n) theps
+    return (ts, js)
+
+main = do
+    rv <- parseOnly specs `fmap` B.getContents
+    case rv of
+        Left e -> print e
+        Right (ts, js) -> print $ hireall ts js
+    where
+    hireall ts js = donate_rejects ts' rejects
+        where (ts', rejects) = round_robin ts js
