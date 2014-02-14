4b37dec779cf8c87a207f0bc99c09ae004c36ebc
bryant <bryant@defrag.in>
2014-02-13 21:18:13 -0500

    basic backtracking sat solver.
    lessons learned:
    - holy crap this was simple
    - know what to program before programming.
      - helps to draw out example cases.
      - don't chase myths in a circle.

---
+++ bool-sat-solver.hs
@@ -0,0 +1,22 @@
+import Debug.Trace (trace)
+
+type Literal = Int
+type Clause = [Literal]
+type SolutionSet = [Literal]
+
+solns `disproves` l = -l `elem` solns
+
+solve' :: SolutionSet -> [Clause] -> Maybe SolutionSet
+solve' solns [] = Just solns
+solve' solns (c:cs) = trace msg $ try_branches c
+    where
+    msg = show solns ++ " -?-> " ++ show (c:cs)
+    try_branches [] = Nothing
+    try_branches (l:ls) = case solns `disproves` l of
+        False -> case solve' solns' cs of
+            Nothing -> try_branches ls
+            Just yay -> Just yay
+            where solns' = if l `elem` solns then solns else l : solns
+        True -> try_branches ls
+
+solve = solve' []
