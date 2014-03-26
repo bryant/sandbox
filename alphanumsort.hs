1e49fe46d0f27d8a5847b75f67def567731bce5e
bryant <bryant@defrag.in>
2014-03-26 19:11:28 -0400

    add the usual test.

--- alphanumsort.hs
+++ alphanumsort.hs
@@ -1,4 +1,4 @@
-import Data.List (takeWhile, dropWhile)
+import Data.List (takeWhile, dropWhile, sortBy)
 import Data.Char (isDigit)
 
 acmp :: String -> String -> Ordering
@@ -18,3 +18,48 @@ acmp xall@(x:xs) yall@(y:ys) = case (take_dig xall, take_dig yall) of
     take_dig = takeWhile isDigit
     drop_dig = dropWhile isDigit
     read_int = read :: (Ord a, Read a, Num a) => String -> a
+
+main = putStrLn . unlines $ sortBy acmp names
+    where
+    names =
+        [ "1000X Radonius Maximus"
+        , "10X Radonius"
+        , "200X Radonius"
+        , "20X Radonius"
+        , "20X Radonius Prime"
+        , "30X Radonius"
+        , "40X Radonius"
+        , "Allegia 50 Clasteron"
+        , "Allegia 500 Clasteron"
+        , "Allegia 51 Clasteron"
+        , "Allegia 51B Clasteron"
+        , "Allegia 52 Clasteron"
+        , "Allegia 60 Clasteron"
+        , "Alpha 100"
+        , "Alpha 2"
+        , "Alpha 200"
+        , "Alpha 2A"
+        , "Alpha 2A-8000"
+        , "Alpha 2A-900"
+        , "Callisto Morphamax"
+        , "Callisto Morphamax 500"
+        , "Callisto Morphamax 5000"
+        , "Callisto Morphamax 600"
+        , "Callisto Morphamax 700"
+        , "Callisto Morphamax 7000"
+        , "Callisto Morphamax 7000 SE"
+        , "Callisto Morphamax 7000 SE2"
+        , "QRS-60 Intrinsia Machine"
+        , "QRS-60F Intrinsia Machine"
+        , "QRS-62 Intrinsia Machine"
+        , "QRS-62F Intrinsia Machine"
+        , "Xiph Xlater 10000"
+        , "Xiph Xlater 2000"
+        , "Xiph Xlater 300"
+        , "Xiph Xlater 40"
+        , "Xiph Xlater 5"
+        , "Xiph Xlater 50"
+        , "Xiph Xlater 500"
+        , "Xiph Xlater 5000"
+        , "Xiph Xlater 58"
+        ]
