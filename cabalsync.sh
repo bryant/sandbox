66ffe2aebade9d7e838d09531b5218e0cbf94d30
bryant <bryant@defrag.in>
2014-04-08 02:07:36 -0400

    cabal-sync: rough procedure.

---
+++ cabalsync.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ $# != 4 ]
+then
+    echo "usage: $0 srclib srcconf destlib destconf"
+    exit
+fi
+
+srclib=$1
+srcconf=$2
+destlib=$3
+destconf=$4
+
+cp -r -t "$destlib" "$srclib"/*
+TEMPDIR=$(mktemp -d)
+cp -t "$TEMPDIR" "$srcconf"/*.conf
+
+for i in "$TEMPDIR"/*.conf
+do
+    pkgname=$(basename $i)
+    sed -i -e 's#^import-dirs: .*$#import-dirs: '"$destlib/$pkgname#g" \
+           -e 's#^library-dirs: .*$#library-dirs: '"$destlib/$pkgname#g" $i
+done
+
+cp -t "$destconf" "$TEMPDIR"/*.conf
+ghc-pkg recache
