83eb21bb763c1ecc24408cbd8cc345cf5c6e5b95
bryant <bryant@defrag.in>
2014-08-25 22:40:01 -0400

    apply clang-format

--- either.cpp
+++ either.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
-template <typename L, typename R>
-struct Either {
-    Either(L left) : val {left}, parity(false) {}
-    Either(R right) : val {right}, parity(true) {}
+template <typename L, typename R> struct Either {
+    Either(L left) : val{left}, parity(false) {}
+    Either(R right) : val{right}, parity(true) {}
 
     static Either<L, R> Left(L left) { return Either(left); }
     static Either<L, R> Right(R right) { return Either(right); }
 
-    friend std::ostream& operator<< (std::ostream &out, const Either &e) {
-        if (e.parity == false) { out << "Left(" << e.val.left; }
-        else { out << "Right(" << e.val.right; }
+    friend std::ostream &operator<<(std::ostream &out, const Either &e) {
+        if (e.parity == false) {
+            out << "Left(" << e.val.left;
+        } else {
+            out << "Right(" << e.val.right;
+        }
         out << ")";
         return out;
     }
