1519e3ec3779a0a71f6cb7a60cd090077cd38e8d
bryant <bryant@defrag.in>
2015-01-02 13:53:07 -0500

    less verbose copyhost; host ip as arg

--- copyhost.sh
+++ copyhost.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-HOST=192.168.1.9
-DIRS=3rd/z .config .ghc .icewm .terminfo .vim
-FILES=.bash_history .bash_logout .bash_profile .bashrc .gdbinit .gitconfig \
-    .inputrc .profile .tmux.conf .vimrc .Xdefaults .Xresources
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 host_ip"
+    exit
+fi
+
+HOST=$1
+DIRS="3rd/z .config .ghc .icewm .terminfo .vim"
+FILES=".bash_history .bash_logout .bash_profile .bashrc .gdbinit .gitconfig \
+    .inputrc .profile .tmux.conf .vimrc .Xdefaults .Xresources"
 
 ssh-copy-id bryant@$HOST && \
-    rsync -aviz --relative $DIRS $FILES bryant@$HOST:~/
+    rsync -aiz --relative $DIRS $FILES bryant@$HOST:~/
