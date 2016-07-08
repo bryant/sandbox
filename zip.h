babf34aa94c59ae58209128e73b0db7d958686ca
bryant <bryant@defrag.in>
2016-07-08 18:16:30 -0400

    more zip iterator fixups

--- zip.h
+++ zip.h
@@ -1,14 +1,8 @@
 #pragma once
 
-#include "llvm/ADT/iterator_range.h"
 #include <algorithm>
 #include <array>
 #include <tuple>
-
-namespace details {
-using llvm::make_range;
-using std::get;
-using std::forward;
 
 // NatList :: [Nat]
 template <unsigned... Ns> struct NatList {};
@@ -32,73 +26,80 @@ template <unsigned max> struct BuildNatList<max, max> {
     using eval = NatList<max>;
 };
 
-template <typename... Iters> struct ZipFirst {
+template <typename... Iters> class ZipFirst {
+  public:
     typedef typename BuildNatList<0, sizeof...(Iters)-1>::eval nat_list;
     typedef std::input_iterator_tag iterator_category;
     typedef std::tuple<decltype(*std::declval<Iters>())...> value_type;
-
     std::tuple<Iters...> iterators;
 
+  private:
     template <unsigned... Ns> value_type deres(NatList<Ns...>) {
-        return value_type(*get<Ns>(iterators)...);
+        return value_type(*std::get<Ns>(iterators)...);
     }
-    value_type operator*() { return deres(nat_list{}); }
 
     template <unsigned... Ns> decltype(iterators) tup_inc(NatList<Ns...>) {
-        return std::tuple<Iters...>(std::next(get<Ns>(iterators))...);
+        return std::tuple<Iters...>(std::next(std::get<Ns>(iterators))...);
     }
+
+  public:
+    value_type operator*() { return deres(nat_list{}); }
+
     void operator++() { iterators = tup_inc(nat_list{}); }
 
     bool operator!=(const ZipFirst<Iters...> &other) const {
-        return get<0>(iterators) != get<0>(other.iterators);
+        return std::get<0>(iterators) != std::get<0>(other.iterators);
     }
-    ZipFirst(Iters &&... ts) : iterators(forward<Iters>(ts)...) {}
+    ZipFirst(Iters &&... ts) : iterators(std::forward<Iters>(ts)...) {}
 };
 
-template <typename... Iters> struct ZipShortest : public ZipFirst<Iters...> {
+template <typename... Iters> class ZipShortest : public ZipFirst<Iters...> {
     template <unsigned... Ns>
     bool test(const ZipFirst<Iters...> &other, NatList<Ns...>) const {
-        std::array<bool, sizeof...(Iters)> conds = {
-            (get<Ns>(this->iterators) != get<Ns>(other.iterators))...};
+        std::array<bool, sizeof...(Iters)> conds = {(
+            std::get<Ns>(this->iterators) != std::get<Ns>(other.iterators))...};
         return std::all_of(conds.begin(), conds.end(),
                            [](bool a) { return a; });
     }
+
+  public:
     bool operator!=(const ZipFirst<Iters...> &other) const {
         return test(other, typename ZipFirst<Iters...>::nat_list{});
     }
-    ZipShortest(Iters &&... ts) : ZipFirst<Iters...>(forward<Iters>(ts)...) {}
+    ZipShortest(Iters &&... ts)
+        : ZipFirst<Iters...>(std::forward<Iters>(ts)...) {}
 };
 
-template <template <typename...> class ItType, typename... Args> struct Zippy {
-    typedef typename BuildNatList<0, sizeof...(Args)-1>::eval nat_list;
+template <template <typename...> class ItType, typename... Args> class Zippy {
+  public:
     typedef ItType<typename std::remove_reference<Args>::type::iterator...>
         iterator;
 
+  private:
+    typedef typename BuildNatList<0, sizeof...(Args)-1>::eval nat_list;
     std::tuple<Args...> ts;
 
     template <unsigned... Ns> iterator begin_impl(NatList<Ns...>) {
-        return iterator(get<Ns>(ts).begin()...);
+        return iterator(std::get<Ns>(ts).begin()...);
     }
     template <unsigned... Ns> iterator end_impl(NatList<Ns...>) {
-        return iterator(get<Ns>(ts).end()...);
+        return iterator(std::get<Ns>(ts).end()...);
     }
+
+  public:
     iterator begin() { return begin_impl(nat_list{}); }
     iterator end() { return end_impl(nat_list{}); }
+    Zippy(Args &&... ts_) : ts(std::forward<Args>(ts_)...) {}
 };
+
+template <typename T, typename U, typename... Args>
+Zippy<ZipFirst, T, U, Args...> zip(T &&t, U &&u, Args &&... args) {
+    return Zippy<ZipFirst, T, U, Args...>(
+        std::forward<T>(t), std::forward<U>(u), std::forward<Args>(args)...);
 }
 
 template <typename T, typename U, typename... Args>
-details::Zippy<details::ZipFirst, T, U, Args...> zip(T &&t, U &&u,
-                                                     Args &&... args) {
-    using namespace details;
-    return Zippy<ZipFirst, T, U, Args...>{std::tuple<T, U, Args...>(
-        forward<T>(t), forward<U>(u), forward<Args>(args)...)};
-}
-
-template <typename T, typename U, typename... Args>
-details::Zippy<details::ZipShortest, T, U, Args...>
-zip_shortest(T &&t, U &&u, Args &&... args) {
-    using namespace details;
-    return Zippy<ZipShortest, T, U, Args...>{std::tuple<T, U, Args...>(
-        forward<T>(t), forward<U>(u), forward<Args>(args)...)};
+Zippy<ZipShortest, T, U, Args...> zip_shortest(T &&t, U &&u, Args &&... args) {
+    return Zippy<ZipShortest, T, U, Args...>(
+        std::forward<T>(t), std::forward<U>(u), std::forward<Args>(args)...);
 }
