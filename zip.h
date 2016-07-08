d97e84002b06dbffef0733dbc8143ee791a34670
bryant <bryant@defrag.in>
2016-07-08 16:10:05 -0400

    fix up zip iterator

--- zip.h
+++ zip.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "llvm/ADT/iterator_range.h"
+#include <algorithm>
+#include <array>
+#include <tuple>
 
 namespace details {
 using llvm::make_range;
 using std::get;
-using std::tuple_size;
-using std::tuple;
-using std::input_iterator_tag;
+using std::forward;
 
 // NatList :: [Nat]
 template <unsigned... Ns> struct NatList {};
@@ -31,47 +32,73 @@ template <unsigned max> struct BuildNatList<max, max> {
     using eval = NatList<max>;
 };
 
-template <typename Tup, unsigned... Ns>
-auto tup_inc(Tup args, NatList<Ns...>)
-    -> decltype(make_tuple(next(get<Ns>(args))...)) {
-    return make_tuple(next(get<Ns>(args))...);
-}
+template <typename... Iters> struct ZipFirst {
+    typedef typename BuildNatList<0, sizeof...(Iters)-1>::eval nat_list;
+    typedef std::input_iterator_tag iterator_category;
+    typedef std::tuple<decltype(*std::declval<Iters>())...> value_type;
 
-template <typename... ArgIters> class ZipIter {
-    typedef typename BuildNatList<0, tuple_size<tuple<ArgIters...>>::value -
-                                         1>::eval nat_list;
-    tuple<ArgIters...> iterators;
-    template <unsigned... Ns>
-    auto deres(NatList<Ns...>) -> tuple<decltype(*get<Ns>(iterators)) &&...> {
-        return tuple<decltype(*get<Ns>(iterators))...>(*get<Ns>(iterators)...);
+    std::tuple<Iters...> iterators;
+
+    template <unsigned... Ns> value_type deres(NatList<Ns...>) {
+        return value_type(*get<Ns>(iterators)...);
     }
+    value_type operator*() { return deres(nat_list{}); }
 
-  public:
-    ZipIter(ArgIters... ts) : iterators(make_tuple(ts...)) {}
-
-    auto operator*() -> decltype(deres(nat_list{})) {
-        return deres(nat_list{});
+    template <unsigned... Ns> decltype(iterators) tup_inc(NatList<Ns...>) {
+        return std::tuple<Iters...>(std::next(get<Ns>(iterators))...);
     }
+    void operator++() { iterators = tup_inc(nat_list{}); }
 
-    void operator++() { iterators = tup_inc(iterators, nat_list{}); }
-
-    bool operator!=(const ZipIter<ArgIters...> &other) const {
+    bool operator!=(const ZipFirst<Iters...> &other) const {
         return get<0>(iterators) != get<0>(other.iterators);
     }
-
-    typedef input_iterator_tag iterator_category;
-    typedef decltype(*std::declval<ZipIter<ArgIters...>>()) value_type;
+    ZipFirst(Iters &&... ts) : iterators(forward<Iters>(ts)...) {}
 };
 
-template <typename... Args> ZipIter<Args...> zip_impl(Args... args) {
-    return ZipIter<Args...>(args...);
-}
+template <typename... Iters> struct ZipShortest : public ZipFirst<Iters...> {
+    template <unsigned... Ns>
+    bool test(const ZipFirst<Iters...> &other, NatList<Ns...>) const {
+        std::array<bool, sizeof...(Iters)> conds = {
+            (get<Ns>(this->iterators) != get<Ns>(other.iterators))...};
+        return std::all_of(conds.begin(), conds.end(),
+                           [](bool a) { return a; });
+    }
+    bool operator!=(const ZipFirst<Iters...> &other) const {
+        return test(other, typename ZipFirst<Iters...>::nat_list{});
+    }
+    ZipShortest(Iters &&... ts) : ZipFirst<Iters...>(forward<Iters>(ts)...) {}
+};
+
+template <template <typename...> class ItType, typename... Args> struct Zippy {
+    typedef typename BuildNatList<0, sizeof...(Args)-1>::eval nat_list;
+    typedef ItType<typename std::remove_reference<Args>::type::iterator...>
+        iterator;
+
+    std::tuple<Args...> ts;
+
+    template <unsigned... Ns> iterator begin_impl(NatList<Ns...>) {
+        return iterator(get<Ns>(ts).begin()...);
+    }
+    template <unsigned... Ns> iterator end_impl(NatList<Ns...>) {
+        return iterator(get<Ns>(ts).end()...);
+    }
+    iterator begin() { return begin_impl(nat_list{}); }
+    iterator end() { return end_impl(nat_list{}); }
+};
 }
 
 template <typename T, typename U, typename... Args>
-auto zip(T &&t, U &&u, Args &&... args) -> decltype(
-    make_range(details::zip_impl(t.begin(), u.begin(), args.begin()...),
-               details::zip_impl(t.end(), u.end(), args.end()...))) {
-    return make_range(details::zip_impl(t.begin(), u.begin(), args.begin()...),
-                      details::zip_impl(t.end(), u.end(), args.end()...));
+details::Zippy<details::ZipFirst, T, U, Args...> zip(T &&t, U &&u,
+                                                     Args &&... args) {
+    using namespace details;
+    return Zippy<ZipFirst, T, U, Args...>{std::tuple<T, U, Args...>(
+        forward<T>(t), forward<U>(u), forward<Args>(args)...)};
+}
+
+template <typename T, typename U, typename... Args>
+details::Zippy<details::ZipShortest, T, U, Args...>
+zip_shortest(T &&t, U &&u, Args &&... args) {
+    using namespace details;
+    return Zippy<ZipShortest, T, U, Args...>{std::tuple<T, U, Args...>(
+        forward<T>(t), forward<U>(u), forward<Args>(args)...)};
 }
