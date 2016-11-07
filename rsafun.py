a23c91e4d2dfe631f08aae7e5b10524547304780
bryant <bryant@users.noreply.github.com>
2016-11-06 19:23:52 -0500

    rsafun: add summary demo

--- rsafun.py
+++ rsafun.py
@@ -34,17 +34,27 @@ def powa(m, e, n):
         pos >>= 1
     return rv
 
-# id_rsa is pem-encoded (base64) asn1 struct:
-# RSAPrivateKey ::= SEQUENCE {
-#   version           Version,
-#   modulus           INTEGER,  -- n
-#   publicExponent    INTEGER,  -- e
-#   privateExponent   INTEGER,  -- d
-#   prime1            INTEGER,  -- p
-#   prime2            INTEGER,  -- q
-#   exponent1         INTEGER,  -- d mod (p-1)
-#   exponent2         INTEGER,  -- d mod (q-1)
-#   coefficient       INTEGER,  -- (inverse of q) mod p
-#   otherPrimeInfos   OtherPrimeInfos OPTIONAL
-# }
-# decode with openssl asn1parse -inform pem < id_rsa
+def demo(privkey, pubkey):
+    from subprocess import Popen, PIPE
+    # id_rsa is pem-encoded (base64) asn1 struct:
+    # decode with openssl asn1parse -inform pem < id_rsa
+    p = Popen('openssl asn1parse -inform pem'.split(), stdin=open(privkey),
+              stdout=PIPE)
+    # RSAPrivateKey ::= SEQUENCE {
+    #   version           Version,
+    #   modulus           INTEGER,  -- n
+    #   publicExponent    INTEGER,  -- e
+    #   privateExponent   INTEGER,  -- d
+    #   prime1            INTEGER,  -- p
+    #   prime2            INTEGER,  -- q
+    #   exponent1         INTEGER,  -- d mod (p-1)
+    #   exponent2         INTEGER,  -- d mod (q-1)
+    #   coefficient       INTEGER,  -- (inverse of q) mod p
+    #   otherPrimeInfos   OtherPrimeInfos OPTIONAL
+    # }
+    d = int(p.stdout.read().splitlines()[4].split(':')[-1], 16)
+    e, n = decode_pubkey(open(pubkey).read().split()[1].decode('base64'))
+    message = 3141592653589
+    print "message:", message
+    print "encrypted:", powa(message, e, n)
+    print "decrypted:", powa(powa(message, e, n), d, n)
